aa7c239887ed9377bc58c2784197062ec1a01186
Ted30 <teddymarcenaje@gmail.com>
2023-04-30 00:51:40 +0800

    example format

--- layout/headers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-?>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-<link rel="stylesheet" href="./css/index.css">
--- pages/Tenant/Unit/Unit.php
+++ pages/Tenant/Unit/Unit.php
@@ -1,24 +1,14 @@
+<?php include '../../../layout/header.php' ?>
+
 <?php
 
-include_once('../../../api/units/units.php');
-
+include('../../../api/units/units.php');
 $units = units_show(1);
-
 print_r($units);
-
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<div class="unit_container">
+    
+</div>
 
-<body>
-
-</body>
-
-</html>
+<?php include '../../../layout/footer.php' ?>
