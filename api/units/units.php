ec11171e06fff35b6ac2370fc8fb7f0e35d76cbe
Ted30 <teddymarcenaje@gmail.com>
2023-04-30 03:47:10 +0800

    added unit features method

--- api/units/units.php
+++ api/units/units.php
@@ -1,31 +1,7 @@
 <?php
 
 include_once('../../../connection/connection.php');
-
-function db_query($sql = '', $msg)
-{
-    $con = connection();
-
-    $res = $con->query($sql) or die($con->error);
-
-    $data = null;
-    $out = null;
-
-    while ($row = $res->fetch_assoc()) {
-        $data[] = $row;
-    }
-
-    $out['data'] = $data;
-    $out['msg'] = '';
-
-    if (!$data) $out['msg'] = $msg;
-
-    // for postman testing
-    // echo json_encode($out);
-
-    return $out;
-}
-
+include_once('../../../functions/db_functions.php');
 
 // fetch all units
 function units_index()
@@ -42,6 +18,42 @@ function units_show($id)
 {
     $sql = "SELECT * FROM `cr_units` WHERE id = '$id'";
     $res = db_query($sql, "No unit found with an id of $id.");
+
+    return $res;
+}
+
+
+// UNIT AMENITIES
+
+//fetch all unit's amenities
+function unit_amenities($id)
+{
+    $sql = "SELECT amenity_id FROM `cr_unit_amenities` WHERE unit_id = '$id'";
+    $res = db_query($sql, "No amenities.");
+
+    return $res;
+}
+//fetch all unit's facilities
+function unit_facilities($id)
+{
+    $sql = "SELECT facility_id FROM `cr_unit_facilities` WHERE unit_id = '$id'";
+    $res = db_query($sql, "No facilities.");
+
+    return $res;
+}
+//fetch all unit's inclusions
+function unit_inclusions($id)
+{
+    $sql = "SELECT inclusion_id FROM `cr_unit_inclusions` WHERE unit_id = '$id'";
+    $res = db_query($sql, "No inclusions.");
+
+    return $res;
+}
+//fetch all unit's rules
+function unit_rules($id)
+{
+    $sql = "SELECT rule_id FROM `cr_unit_rules` WHERE unit_id = '$id'";
+    $res = db_query($sql, "No rules.");
 
     return $res;
 }
