3ef5a798e80e3527a50ed6e2a00adc430164e6d9
Ted30 <teddymarcenaje@gmail.com>
2023-04-30 20:50:17 +0800

    updated example

---
+++ modules/tenant/pages/unit_details.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/cr_demo/css/index.css">
+    <link rel="stylesheet" href="../css/index.css">
+</head>
+
+<body>
+    <h2>Hi Johns</h2>
+    <p class="title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi aliquam id perspiciatis non nisi reprehenderit ipsam recusandae impedit reiciendis, perferendis vitae expedita accusamus unde numquam voluptatum. Ut consectetur perferendis dolores a at placeat quas quia, quam nulla aliquam nesciunt aspernatur.</p>
+    <div class="green-box">
+
+    </div>
+</body>
+
+</html>
