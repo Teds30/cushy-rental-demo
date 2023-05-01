d202a5a29369b5fe649a268109656d9365536a7f
Ted30 <teddymarcenaje@gmail.com>
2023-05-02 04:28:16 +0800

    added amenity tab

--- modules/admin/layout/sidebar.php
+++ modules/admin/layout/sidebar.php
@@ -66,7 +66,7 @@
     <p class="pre-title">ATTRIBUTES</p>
 
     <div class="sb-item-group">
-        <a class="sb-item <?php if ($selected == 'amenities') echo 'sb-selected' ?> " href="amenities.php">
+        <a class="sb-item <?php if ($selected == 'amenities') echo 'sb-selected' ?> " href="manage_amenities.php">
             <div class="sb-item-icon"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-sitemap" width="24" height="24" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                     <path d="M3 15m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v2a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z"></path>
