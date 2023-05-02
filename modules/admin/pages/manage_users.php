71d634ee2c4518ad33adc6bfe505578eb9b08079
Arjie2002 <johnarjiedalisay.beredo@bicol-u.edu.ph>
2023-05-02 09:58:07 +0800

    Landlord homepage

--- modules/admin/pages/manage_users.php
+++ modules/admin/pages/manage_users.php
@@ -11,7 +11,7 @@ $selected = 'manage_users';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Manage Users</title>
+    <title>Home Page - Landlord</title>
     <link rel="stylesheet" href="/cr_demo/css/index.css">
     <link rel="stylesheet" href="../css/index.css">
 </head>
---
+++ modules/landlord/pages/home_page.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page - Landlord</title>
+    <link rel="stylesheet" href="/cr_demo/css/index.css">
+    <link rel="stylesheet" href="../css/index.css">
+</head>
+<body>
+    <div class="main-container">
+
+    <img src="/cr_demo/assets/logo/cr_light_24.svg" id="logo_img">
+
+    <h2>CushyRental</h2>
+
+    <div class="main-box_container">
+
+        <div class="sub-box_container">
+
+            <div class="box-1_container">
+
+                <div class=top-container>
+
+                    <div class="top">
+
+                        <div class="gradient">
+                            <div class="gradient-shape">
+                                <div class="gradient-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-community" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8"></path>
+                                        <path d="M13 7l0 .01"></path>
+                                        <path d="M17 7l0 .01"></path>
+                                        <path d="M17 11l0 .01"></path>
+                                        <path d="M17 15l0 .01"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div class="top-text">
+                            <p class="top-text-1">Units</p>
+                            <p class="top-text-2">12</p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="inner-items_container">
+                    <div class="inner-items_sub">
+                        <div>
+                            <div>7</div>
+                            <div>Listed</div>
+                        </div>
+
+                        <div>
+                            <div>3</div>
+                            <div>Unlisted</div>
+                        </div>
+
+                        <div>
+                            <div>1</div>
+                            <div>Occupied</div>
+                        </div>
+
+                        <div>
+                            <div>1</div>
+                            <div>Pending</div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+                <div class="bottom">
+                    <p>Manage Units</p>
+                    <div class="arrow-btn">
+                        <button><i class="fa-sharp fa-solid fa-arrow-right" style="color: #fafafa;"></i></button>
+                    </div> 
+                </div>
+            </div>
+
+            <div class="box-2_container">
+
+                <div class=top-container>
+
+                    <div class="top">
+
+                        <div class="gradient">
+                            <div class="gradient-shape">
+                                <div class="gradient-icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-building-community" width="24" height="24" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                        <path d="M8 9l5 5v7h-5v-4m0 4h-5v-7l5 -5m1 1v-6a1 1 0 0 1 1 -1h10a1 1 0 0 1 1 1v17h-8"></path>
+                                        <path d="M13 7l0 .01"></path>
+                                        <path d="M17 7l0 .01"></path>
+                                        <path d="M17 11l0 .01"></path>
+                                        <path d="M17 15l0 .01"></path>
+                                    </svg>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="top-text">
+                            <p class="top-text-1">Tenants</p>
+                            <p class="top-text-2">3</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="bottom">
+                    <p>Manage Tenants</p>
+                    <div class="arrow-btn">
+                        <button><i class="fa-sharp fa-solid fa-arrow-right" style="color: #fafafa;"></i></button>
+                    </div>       
+                </div>
+
+            </div>
+
+        </div>
+        
+
+        <div class="bottom-box_container">
+            <p>Upcoming Due Date</p>
+
+            <div class="sub_bottom-box_container">
+                
+                <div class="content-1">
+                    <p class="content-1_txt">FRI</p>
+                    <p class="content-1_no">28</p>
+                </div>
+
+                <div class="right-side">
+                    <div class="content-2">
+                        <p class="content-2_txt">Coco's Boarding House</p>
+                        <p class="content-2_no">₱5,000</p>
+                        
+                    </div>
+                    <div class="content-3">
+                        <img src="/cr_demo/modules/landlord/img/img1.jpg">
+                        <p class="content-3_txt">Tiyo Marcus</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+
+    </div>
+    
+
+</body>
+</html>
