f8092e3c9e1926cb80f1f78a3b3004e5cfba6bca
Arjie2002 <johnarjiedalisay.beredo@bicol-u.edu.ph>
2023-05-05 22:52:27 +0800

    Landlord

---
+++ modules/landlord/pages/manage_renters.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manage Renters - Tenants List</title>
+
+    <link rel="stylesheet" href="/cr_demo/css/index.css">
+    <link rel="stylesheet" href="/cr_demo/css/manage_units/index.css">
+    <link rel="stylesheet" href="../css/manage_renters/index.css">
+</head>
+
+<body>
+
+    <nav class="nav">
+        <div class="nav-back">
+            <a href="#">
+                <i class="bi bi-chevron-left"></i>
+            </a>
+        </div>
+
+        <div class="nav-title">
+            <h2 class="title">Manage Renters</h2>
+        </div>
+    </nav>
+
+    <div class="filter-container">
+        <div class="filter-1">
+            <button class="title">Tenants</button>
+        </div>
+
+        <div class="filter-2">
+            <button class="title">Pending Inquiries</button>
+        </div>
+    </div>
+
+    <div class="search-box">
+        <input type="text" class="search-input" placeholder="Search">
+    </div>
+
+    <div class="main-box_container">
+        <div class="box-container">
+            <div class="box-image">
+                <img src="/cr_demo/modules/landlord/img/img2.jpg">
+            </div>
+            <div class="box-details">
+                <p class="box-details-name">Leonardo Dicaprio</p>
+                <p class="box-details-gender">Male</p>
+                <p class="box-details-time">18/04/2023 | 1:00 pm</p>
+            </div>
+        </div>
+
+        <div class="box-container">
+            <div class="box-image">
+                <img src="/cr_demo/modules/landlord/img/img2.jpg">
+            </div>
+            <div class="box-details">
+                <p class="box-details-name">Leonardo Dicaprio</p>
+                <p class="box-details-gender">Male</p>
+                <p class="box-details-time">18/04/2023 | 1:00 pm</p>
+            </div>
+        </div>
+
+        <div class="box-container">
+            <div class="box-image">
+                <img src="/cr_demo/modules/landlord/img/img2.jpg">
+            </div>
+            <div class="box-details">
+                <p class="box-details-name">Leonardo Dicaprio</p>
+                <p class="box-details-gender">Male</p>
+                <p class="box-details-time">18/04/2023 | 1:00 pm</p>
+            </div>
+        </div>
+
+        <div class="box-container">
+            <div class="box-image">
+                <img src="/cr_demo/modules/landlord/img/img2.jpg">
+            </div>
+            <div class="box-details">
+                <p class="box-details-name">Leonardo Dicaprio</p>
+                <p class="box-details-gender">Male</p>
+                <p class="box-details-time">18/04/2023 | 1:00 pm</p>
+            </div>
+        </div>
+
+        <div class="box-container">
+            <div class="box-image">
+                <img src="/cr_demo/modules/landlord/img/img2.jpg">
+            </div>
+            <div class="box-details">
+                <p class="box-details-name">Leonardo Dicaprio</p>
+                <p class="box-details-gender">Male</p>
+                <p class="box-details-time">18/04/2023 | 1:00 pm</p>
+            </div>
+        </div>
+
+
+    </div>
+</body>
+
+</html>
--- modules/landlord/pages/my_unit-landlord.php
+++ modules/landlord/pages/my_unit-landlord.php
@@ -1,38 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Landlord Page - My Unit</title>
+
     <link rel="stylesheet" href="/cr_demo/css/index.css">
+    <link rel="stylesheet" href="/cr_demo/css/manage_units/index.css">
     <link rel="stylesheet" href="../css/my_unit-landlord/index.css">
 </head>
-<body>
-    <div class="header">
-        <button><a href=""></a>
-            <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M14.5808 2.05479C14.634 2.11195 14.6762 2.17985 14.705 2.25461C14.7338 2.32936 14.7487 2.4095 14.7487 2.49044C14.7487 2.57137 14.7338 2.65151 14.705 2.72627C14.6762 2.80102 14.634 2.86892 14.5808 2.92608L8.12875 9.87418L14.5808 16.8223C14.6881 16.9378 14.7484 17.0945 14.7484 17.2579C14.7484 17.4213 14.6881 17.578 14.5808 17.6936C14.4736 17.8091 14.3281 17.874 14.1764 17.874C14.0246 17.874 13.8792 17.8091 13.7719 17.6936L6.91649 10.3098C6.86329 10.2527 6.82108 10.1848 6.79228 10.11C6.76348 10.0353 6.74866 9.95512 6.74866 9.87418C6.74866 9.79325 6.76348 9.71311 6.79228 9.63836C6.82108 9.5636 6.86329 9.4957 6.91649 9.43854L13.7719 2.05479C13.825 1.99749 13.888 1.95203 13.9574 1.92101C14.0268 1.88999 14.1012 1.87402 14.1764 1.87402C14.2515 1.87402 14.3259 1.88999 14.3953 1.92101C14.4647 1.95203 14.5278 1.99749 14.5808 2.05479Z" fill="black"/>
-            </svg>
-        </button>
 
-        <p>My Unit</p>
+<body>
+    <div class="header-container">
+        <div class="header">
+            <div class="nav-back">
+                <a href="#">
+                    <i class="bi bi-chevron-left"></i>
+                </a>
+            </div>
+            <p>My Unit</p>
+        </div>
     </div>
 
     <div class="background-img_container">
         <div class="background-img-1">
             <svg width="360" height="190" viewBox="0 0 360 190" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 0H360.327C360.327 0 406.841 110.249 360.327 164.865C285.814 252.359 0 76.2097 0 164.865C0 253.52 0 0 0 0Z" fill="#1D6156"/>
+                <path d="M0 0H360.327C360.327 0 406.841 110.249 360.327 164.865C285.814 252.359 0 76.2097 0 164.865C0 253.52 0 0 0 0Z" fill="#1D6156" />
             </svg>
         </div>
         <div class="background-img-2">
             <svg width="360" height="190" viewBox="0 0 360 190" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 0H360.327C360.327 0 406.841 110.249 360.327 164.865C285.814 252.359 0 76.2097 0 164.865C0 253.52 0 0 0 0Z" fill="#1D6156"/>
+                <path d="M0 0H360.327C360.327 0 406.841 110.249 360.327 164.865C285.814 252.359 0 76.2097 0 164.865C0 253.52 0 0 0 0Z" fill="#1D6156" />
             </svg>
         </div>
         <div class="background-img-3">
             <svg width="360" height="190" viewBox="0 0 360 190" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0 0H360.327C360.327 0 406.841 110.249 360.327 164.865C285.814 252.359 0 76.2097 0 164.865C0 253.52 0 0 0 0Z" fill="#1D6156"/>
+                <path d="M0 0H360.327C360.327 0 406.841 110.249 360.327 164.865C285.814 252.359 0 76.2097 0 164.865C0 253.52 0 0 0 0Z" fill="#1D6156" />
             </svg>
         </div>
     </div>
@@ -41,31 +46,31 @@
         <a href="#" class="btn-link box-1_container">
             <div class="box-1">
                 <svg width="55" height="54" viewBox="0 0 55 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#clip0_808_3848)">
-                <path d="M24.125 47.25C24.125 47.25 20.75 47.25 20.75 43.875C20.75 40.5 24.125 30.375 37.625 30.375C51.125 30.375 54.5 40.5 54.5 43.875C54.5 47.25 51.125 47.25 51.125 47.25H24.125ZM37.625 27C40.3103 27 42.8857 25.9333 44.7845 24.0345C46.6833 22.1356 47.75 19.5603 47.75 16.875C47.75 14.1897 46.6833 11.6143 44.7845 9.71554C42.8857 7.81674 40.3103 6.75 37.625 6.75C34.9397 6.75 32.3643 7.81674 30.4655 9.71554C28.5667 11.6143 27.5 14.1897 27.5 16.875C27.5 19.5603 28.5667 22.1356 30.4655 24.0345C32.3643 25.9333 34.9397 27 37.625 27Z" fill="white"/>
-                <path fill-rule="evenodd" clip-rule="evenodd" d="M18.104 47.25C17.6037 46.1964 17.3542 45.0412 17.375 43.875C17.375 39.3019 19.67 34.5937 23.909 31.32C21.7932 30.668 19.5888 30.3492 17.375 30.375C3.875 30.375 0.5 40.5 0.5 43.875C0.5 47.25 3.875 47.25 3.875 47.25H18.104Z" fill="white"/>
-                <path d="M15.6875 27C17.9253 27 20.0714 26.1111 21.6537 24.5287C23.2361 22.9464 24.125 20.8003 24.125 18.5625C24.125 16.3247 23.2361 14.1786 21.6537 12.5963C20.0714 11.0139 17.9253 10.125 15.6875 10.125C13.4497 10.125 11.3036 11.0139 9.72129 12.5963C8.13895 14.1786 7.25 16.3247 7.25 18.5625C7.25 20.8003 8.13895 22.9464 9.72129 24.5287C11.3036 26.1111 13.4497 27 15.6875 27Z" fill="white"/>
-                </g>
-                <defs>
-                <clipPath id="clip0_808_3848">
-                <rect width="54" height="54" fill="white" transform="translate(0.5)"/>
-                </clipPath>
-                </defs>
+                    <g clip-path="url(#clip0_808_3848)">
+                        <path d="M24.125 47.25C24.125 47.25 20.75 47.25 20.75 43.875C20.75 40.5 24.125 30.375 37.625 30.375C51.125 30.375 54.5 40.5 54.5 43.875C54.5 47.25 51.125 47.25 51.125 47.25H24.125ZM37.625 27C40.3103 27 42.8857 25.9333 44.7845 24.0345C46.6833 22.1356 47.75 19.5603 47.75 16.875C47.75 14.1897 46.6833 11.6143 44.7845 9.71554C42.8857 7.81674 40.3103 6.75 37.625 6.75C34.9397 6.75 32.3643 7.81674 30.4655 9.71554C28.5667 11.6143 27.5 14.1897 27.5 16.875C27.5 19.5603 28.5667 22.1356 30.4655 24.0345C32.3643 25.9333 34.9397 27 37.625 27Z" fill="white" />
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M18.104 47.25C17.6037 46.1964 17.3542 45.0412 17.375 43.875C17.375 39.3019 19.67 34.5937 23.909 31.32C21.7932 30.668 19.5888 30.3492 17.375 30.375C3.875 30.375 0.5 40.5 0.5 43.875C0.5 47.25 3.875 47.25 3.875 47.25H18.104Z" fill="white" />
+                        <path d="M15.6875 27C17.9253 27 20.0714 26.1111 21.6537 24.5287C23.2361 22.9464 24.125 20.8003 24.125 18.5625C24.125 16.3247 23.2361 14.1786 21.6537 12.5963C20.0714 11.0139 17.9253 10.125 15.6875 10.125C13.4497 10.125 11.3036 11.0139 9.72129 12.5963C8.13895 14.1786 7.25 16.3247 7.25 18.5625C7.25 20.8003 8.13895 22.9464 9.72129 24.5287C11.3036 26.1111 13.4497 27 15.6875 27Z" fill="white" />
+                    </g>
+                    <defs>
+                        <clipPath id="clip0_808_3848">
+                            <rect width="54" height="54" fill="white" transform="translate(0.5)" />
+                        </clipPath>
+                    </defs>
                 </svg>
 
                 <p>MANAGE RENTERS</p>
             </div>
         </a>
-        
+
         <div class="box-2_container">
             <div class="box-2">
 
                 <svg width="55" height="54" viewBox="0 0 55 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M29.75 47.25V31.5L18.5 20.25L7.25 31.5V47.25H18.5M29.75 47.25H18.5M29.75 47.25H47.75V9C47.75 8.40326 47.5129 7.83097 47.091 7.40901C46.669 6.98705 46.0967 6.75 45.5 6.75H23C22.4033 6.75 21.831 6.98705 21.409 7.40901C20.9871 7.83097 20.75 8.40326 20.75 9V22.5M18.5 47.25V38.25" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M29.75 15.75V15.7725" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M38.75 15.75V15.7725" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M38.75 24.75V24.7725" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                <path d="M38.75 33.75V33.7725" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M29.75 47.25V31.5L18.5 20.25L7.25 31.5V47.25H18.5M29.75 47.25H18.5M29.75 47.25H47.75V9C47.75 8.40326 47.5129 7.83097 47.091 7.40901C46.669 6.98705 46.0967 6.75 45.5 6.75H23C22.4033 6.75 21.831 6.98705 21.409 7.40901C20.9871 7.83097 20.75 8.40326 20.75 9V22.5M18.5 47.25V38.25" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M29.75 15.75V15.7725" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M38.75 15.75V15.7725" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M38.75 24.75V24.7725" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M38.75 33.75V33.7725" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                 </svg>
 
                 <p>MANAGE UNITS</p>
@@ -73,6 +78,6 @@
         </div>
 
     </div>
-
 </body>
+
 </html>
