8ac8ee04ae74552857a3eacba6f86eb5feb5d569
Arjie2002 <johnarjiedalisay.beredo@bicol-u.edu.ph>
2023-05-06 21:42:49 +0800

    Manage Unit - Location

---
+++ modules/landlord/pages/manage_unit-location.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manage Units - Location</title>
+
+    <link rel="stylesheet" href="/cr_demo/css/index.css">
+    <link rel="stylesheet" href="/cr_demo/css/manage_units/index.css">
+    <link rel="stylesheet" href="../css/manage_unit-location/index.css">
+</head>
+
+<body>
+    <nav class="nav">
+        <div class="nav-back">
+            <a href="#">
+                <i class="bi bi-chevron-left"></i>
+            </a>
+        </div>
+
+        <div class="nav-title">
+            <h2 class="title">Location</h2>
+        </div>
+    </nav>
+
+    <div class="main-container">
+        <div class="search-box">
+            <input type="text" class="search-input" placeholder="Search">
+        </div>
+
+        <div class="map-box"></div>
+    </div>
+
+
+
+
+
+</body>
+
+</html>
