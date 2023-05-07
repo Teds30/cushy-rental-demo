ed684f20cb6245c6886b8e02e465fa3b8d522827
Arjie2002 <johnarjiedalisay.beredo@bicol-u.edu.ph>
2023-05-08 01:41:07 +0800

    landlord subscription page and other minor changes

---
+++ modules/landlord/pages/subscription-page1.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subscription - Page 1</title>
+
+    <link rel="stylesheet" href="/cr_demo/css/index.css">
+    <link rel="stylesheet" href="../css/subscription-page1/index.css">
+</head>
+
+<body>
+
+    <div class="background-img-top"></div>
+
+    <div class="header">
+        <nav class="nav">
+            <div class="nav-back">
+                <a href="">
+                    <i class="bi bi-chevron-left"></i>
+                </a>
+            </div>
+        </nav>
+        <p>Subscription</p>
+    </div>
+
+    <!-- <div class="circle">
+
+    </div> -->
+
+    <div class="step-container">
+        <div class="step1-container">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_564_3156)">
+                    <path d="M12 22.5C9.21523 22.5 6.54451 21.3938 4.57538 19.4246C2.60625 17.4555 1.5 14.7848 1.5 12C1.5 9.21523 2.60625 6.54451 4.57538 4.57538C6.54451 2.60625 9.21523 1.5 12 1.5C14.7848 1.5 17.4555 2.60625 19.4246 4.57538C21.3938 6.54451 22.5 9.21523 22.5 12C22.5 14.7848 21.3938 17.4555 19.4246 19.4246C17.4555 21.3938 14.7848 22.5 12 22.5ZM12 24C15.1826 24 18.2348 22.7357 20.4853 20.4853C22.7357 18.2348 24 15.1826 24 12C24 8.8174 22.7357 5.76516 20.4853 3.51472C18.2348 1.26428 15.1826 0 12 0C8.8174 0 5.76516 1.26428 3.51472 3.51472C1.26428 5.76516 0 8.8174 0 12C0 15.1826 1.26428 18.2348 3.51472 20.4853C5.76516 22.7357 8.8174 24 12 24Z" fill="#03B077" />
+                    <path d="M12.8334 18.6666V5.33325L8.66675 9.49992" stroke="#03B077" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </g>
+                <defs>
+                    <clipPath id="clip0_564_3156">
+                        <rect width="24" height="24" fill="white" />
+                    </clipPath>
+                </defs>
+            </svg>
+        </div>
+        <div class="hr-container-green"></div>
+        <div class="step2-container">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_564_3160)">
+                    <path d="M12 22.5C9.21523 22.5 6.54451 21.3938 4.57538 19.4246C2.60625 17.4555 1.5 14.7848 1.5 12C1.5 9.21523 2.60625 6.54451 4.57538 4.57538C6.54451 2.60625 9.21523 1.5 12 1.5C14.7848 1.5 17.4555 2.60625 19.4246 4.57538C21.3938 6.54451 22.5 9.21523 22.5 12C22.5 14.7848 21.3938 17.4555 19.4246 19.4246C17.4555 21.3938 14.7848 22.5 12 22.5ZM12 24C15.1826 24 18.2348 22.7357 20.4853 20.4853C22.7357 18.2348 24 15.1826 24 12C24 8.8174 22.7357 5.76516 20.4853 3.51472C18.2348 1.26428 15.1826 0 12 0C8.8174 0 5.76516 1.26428 3.51472 3.51472C1.26428 5.76516 0 8.8174 0 12C0 15.1826 1.26428 18.2348 3.51472 20.4853C5.76516 22.7357 8.8174 24 12 24Z" fill="#03B077" />
+                    <path d="M8.66675 8.66659C8.66675 7.78253 9.01794 6.93468 9.64306 6.30956C10.2682 5.68444 11.116 5.33325 12.0001 5.33325C12.8841 5.33325 13.732 5.68444 14.3571 6.30956C14.9822 6.93468 15.3334 7.78253 15.3334 8.66659C15.3334 9.58159 14.8634 10.3541 14.3676 11.0124L8.66675 18.6666H15.3334" stroke="#03B077" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </g>
+                <defs>
+                    <clipPath id="clip0_564_3160">
+                        <rect width="24" height="24" fill="white" />
+                    </clipPath>
+                </defs>
+            </svg>
+
+        </div>
+        <div class="hr-container-green"></div>
+        <div class="step3-container">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g clip-path="url(#clip0_564_3164)">
+                    <path d="M12 22.5C9.21523 22.5 6.54451 21.3938 4.57538 19.4246C2.60625 17.4555 1.5 14.7848 1.5 12C1.5 9.21523 2.60625 6.54451 4.57538 4.57538C6.54451 2.60625 9.21523 1.5 12 1.5C14.7848 1.5 17.4555 2.60625 19.4246 4.57538C21.3938 6.54451 22.5 9.21523 22.5 12C22.5 14.7848 21.3938 17.4555 19.4246 19.4246C17.4555 21.3938 14.7848 22.5 12 22.5ZM12 24C15.1826 24 18.2348 22.7357 20.4853 20.4853C22.7357 18.2348 24 15.1826 24 12C24 8.8174 22.7357 5.76516 20.4853 3.51472C18.2348 1.26428 15.1826 0 12 0C8.8174 0 5.76516 1.26428 3.51472 3.51472C1.26428 5.76516 0 8.8174 0 12C0 15.1826 1.26428 18.2348 3.51472 20.4853C5.76516 22.7357 8.8174 24 12 24Z" fill="#03B077" />
+                    <path d="M12.0001 11.9999C12.6594 11.9999 13.3038 11.8044 13.852 11.4382C14.4001 11.0719 14.8274 10.5513 15.0797 9.9422C15.332 9.33311 15.398 8.66289 15.2694 8.01629C15.1407 7.36968 14.8233 6.77574 14.3571 6.30956C13.8909 5.84339 13.297 5.52592 12.6504 5.3973C12.0038 5.26869 11.3336 5.3347 10.7245 5.58699C10.1154 5.83928 9.59479 6.26652 9.22852 6.81469C8.86224 7.36285 8.66675 8.00732 8.66675 8.66659" stroke="#03B077" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M8.66675 15.3333C8.66675 15.9926 8.86224 16.6371 9.22852 17.1852C9.59479 17.7334 10.1154 18.1606 10.7245 18.4129C11.3336 18.6652 12.0038 18.7312 12.6504 18.6026C13.297 18.474 13.8909 18.1565 14.3571 17.6904C14.8233 17.2242 15.1407 16.6302 15.2694 15.9836C15.398 15.337 15.332 14.6668 15.0797 14.0577C14.8274 13.4486 14.4001 12.928 13.852 12.5618C13.3038 12.1955 12.6594 12 12.0001 12" stroke="#03B077" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                </g>
+                <defs>
+                    <clipPath id="clip0_564_3164">
+                        <rect width="24" height="24" fill="white" />
+                    </clipPath>
+                </defs>
+            </svg>
+
+        </div>
+
+
+
+    </div>
+
+
+    <div class="main-box-container">
+        <p class="caption">Please scan the QR code provided and follow the payment instructions. Or pay directly to the given number. Make sure to input the correct amount and verify the payment details before proceeding with the payment.</p>
+
+        <div class="box-container">
+            <p>QR Code</p>
+
+            <img src="../img/qrcode.jpg" alt="">
+
+            <div class="box-bottom-container">
+                <div class="hr-container"></div>
+                <div class="text-container">
+                    <p class="caption">Or through this number</p>
+                </div>
+                <div class="hr-container"></div>
+            </div>
+
+
+
+
+            <p class="number title">+639123456789</p>
+        </div>
+
+
+        <button class="btn btn-primary btn-fill" type="submit">Next
+        </button>
+    </div>
+</body>
+
+</html>
---
+++ modules/landlord/pages/subscription.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subscription</title>
+
+    <link rel="stylesheet" href="/cr_demo/css/index.css">
+    <link rel="stylesheet" href="../css/subscription/index.css">
+</head>
+
+<body>
+    <nav class="nav">
+        <div class="nav-back">
+            <a href="">
+                <i class="bi bi-chevron-left"></i>
+            </a>
+        </div>
+    </nav>
+
+    <div class="headertext-container">
+        <p>Subscription</p>
+
+        <p class="pre title">THE RIGHT PLAN FOR YOUR BUSINESS</p>
+
+        <p class="caption">Unlock the full potential of your rental business with our subscription plans - the ultimate solution for landlords looking to showcase their properties and attract the perfect tenants!</p>
+
+    </div>
+</body>
+
+</html>
