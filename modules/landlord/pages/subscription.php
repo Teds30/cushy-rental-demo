5b23c0eddee415ef83916b5b824fc76beeb949b3
Arjie2002 <johnarjiedalisay.beredo@bicol-u.edu.ph>
2023-05-08 13:08:12 +0800

    Landlord - Subscription Slider

--- modules/landlord/pages/subscription.php
+++ modules/landlord/pages/subscription.php
@@ -25,7 +25,180 @@
 
         <p class="pre title">THE RIGHT PLAN FOR YOUR BUSINESS</p>
 
+
         <p class="caption">Unlock the full potential of your rental business with our subscription plans - the ultimate solution for landlords looking to showcase their properties and attract the perfect tenants!</p>
+
+
+        <div class="container">
+
+            <input type="radio" name="slider" class="d-none" id="s1" checked>
+            <input type="radio" name="slider" class="d-none" id="s2">
+            <input type="radio" name="slider" class="d-none" id="s3">
+
+            <div class="cards">
+                <label for="s1" id="slide1">
+                    <div class="card-container">
+                        <div class="card-top-text">
+                            <div class="card-top-text-icon">
+                                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="16.1477" cy="16.2939" r="15.5" fill="#C0C0C0" />
+                                    <path d="M18.1213 18.5185L16.6123 22.6683C16.5753 22.7679 16.5087 22.8538 16.4215 22.9145C16.3343 22.9751 16.2306 23.0076 16.1244 23.0076C16.0182 23.0076 15.9145 22.9751 15.8273 22.9145C15.7401 22.8538 15.6735 22.7679 15.6366 22.6683L14.1275 18.5185C14.1012 18.4469 14.0596 18.382 14.0057 18.3281C13.9518 18.2742 13.8869 18.2326 13.8153 18.2063L9.66545 16.6972C9.56587 16.6603 9.47998 16.5937 9.41933 16.5065C9.35868 16.4193 9.32617 16.3156 9.32617 16.2094C9.32617 16.1032 9.35868 15.9995 9.41933 15.9123C9.47998 15.8251 9.56587 15.7585 9.66545 15.7215L13.8153 14.2125C13.8869 14.1861 13.9518 14.1446 14.0057 14.0907C14.0596 14.0368 14.1012 13.9718 14.1275 13.9003L15.6366 9.75041C15.6735 9.65083 15.7401 9.56494 15.8273 9.50429C15.9145 9.44364 16.0182 9.41113 16.1244 9.41113C16.2306 9.41113 16.3343 9.44364 16.4215 9.50429C16.5087 9.56494 16.5753 9.65083 16.6123 9.75041L18.1213 13.9003C18.1477 13.9718 18.1892 14.0368 18.2431 14.0907C18.297 14.1446 18.362 14.1861 18.4335 14.2125L22.5834 15.7215C22.683 15.7585 22.7689 15.8251 22.8295 15.9123C22.8902 15.9995 22.9227 16.1032 22.9227 16.2094C22.9227 16.3156 22.8902 16.4193 22.8295 16.5065C22.7689 16.5937 22.683 16.6603 22.5834 16.6972L18.4335 18.2063C18.362 18.2326 18.297 18.2742 18.2431 18.3281C18.1892 18.382 18.1477 18.4469 18.1213 18.5185Z" fill="#C0C0C0" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
+                                <p class="title">BRONZE</p>
+                            </div>
+
+                            <p class="caption">Lorem Ipsum</p>
+                        </div>
+
+                        <div class="card-price-text">
+                            <p class="peso">₱</p>
+                            <p class="price">199</p>
+                            <p class="month">/month</p>
+                        </div>
+
+                        <button class="btn btn-primary btn-fill btn-contact">Subscribe
+                        </button>
+
+                        <hr>
+
+                        <div class="card-details-text-container">
+                            <div class="card-details-text">
+                                <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.2925 2.39282L5.11895 11.3265L2.47314 7.97636" stroke="#2FCF3F" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
+                                <p class="pre-title">Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+                            </div>
+                            <div class="card-details-text">
+                                <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.2925 2.39282L5.11895 11.3265L2.47314 7.97636" stroke="#2FCF3F" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
+                                <p class="pre-title">Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+                            </div>
+                            <div class="card-details-text">
+                                <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.2925 2.39282L5.11895 11.3265L2.47314 7.97636" stroke="#2FCF3F" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
+                                <p class="pre-title">Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </label>
+
+                <label for="s2" id="slide2">
+                    <div class="card-container">
+                        <div class="card-top-text">
+                            <div class="card-top-text-icon">
+                                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="16.1477" cy="16.2939" r="15.5" fill="#C0C0C0" />
+                                    <path d="M18.1213 18.5185L16.6123 22.6683C16.5753 22.7679 16.5087 22.8538 16.4215 22.9145C16.3343 22.9751 16.2306 23.0076 16.1244 23.0076C16.0182 23.0076 15.9145 22.9751 15.8273 22.9145C15.7401 22.8538 15.6735 22.7679 15.6366 22.6683L14.1275 18.5185C14.1012 18.4469 14.0596 18.382 14.0057 18.3281C13.9518 18.2742 13.8869 18.2326 13.8153 18.2063L9.66545 16.6972C9.56587 16.6603 9.47998 16.5937 9.41933 16.5065C9.35868 16.4193 9.32617 16.3156 9.32617 16.2094C9.32617 16.1032 9.35868 15.9995 9.41933 15.9123C9.47998 15.8251 9.56587 15.7585 9.66545 15.7215L13.8153 14.2125C13.8869 14.1861 13.9518 14.1446 14.0057 14.0907C14.0596 14.0368 14.1012 13.9718 14.1275 13.9003L15.6366 9.75041C15.6735 9.65083 15.7401 9.56494 15.8273 9.50429C15.9145 9.44364 16.0182 9.41113 16.1244 9.41113C16.2306 9.41113 16.3343 9.44364 16.4215 9.50429C16.5087 9.56494 16.5753 9.65083 16.6123 9.75041L18.1213 13.9003C18.1477 13.9718 18.1892 14.0368 18.2431 14.0907C18.297 14.1446 18.362 14.1861 18.4335 14.2125L22.5834 15.7215C22.683 15.7585 22.7689 15.8251 22.8295 15.9123C22.8902 15.9995 22.9227 16.1032 22.9227 16.2094C22.9227 16.3156 22.8902 16.4193 22.8295 16.5065C22.7689 16.5937 22.683 16.6603 22.5834 16.6972L18.4335 18.2063C18.362 18.2326 18.297 18.2742 18.2431 18.3281C18.1892 18.382 18.1477 18.4469 18.1213 18.5185Z" fill="#C0C0C0" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
+                                <p class="title">BRONZE</p>
+                            </div>
+
+                            <p class="caption">Lorem Ipsum</p>
+                        </div>
+
+                        <div class="card-price-text">
+                            <p class="peso">₱</p>
+                            <p class="price">199</p>
+                            <p class="month">/month</p>
+                        </div>
+
+                        <button class="btn btn-primary btn-fill btn-contact">Subscribe
+                        </button>
+
+                        <hr>
+
+                        <div class="card-details-text-container">
+                            <div class="card-details-text">
+                                <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.2925 2.39282L5.11895 11.3265L2.47314 7.97636" stroke="#2FCF3F" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
+                                <p class="pre-title">Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+                            </div>
+                            <div class="card-details-text">
+                                <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.2925 2.39282L5.11895 11.3265L2.47314 7.97636" stroke="#2FCF3F" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
+                                <p class="pre-title">Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+                            </div>
+                            <div class="card-details-text">
+                                <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.2925 2.39282L5.11895 11.3265L2.47314 7.97636" stroke="#2FCF3F" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
+                                <p class="pre-title">Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </label>
+
+                <label for="s3" id="slide3">
+                    <div class="card-container">
+                        <div class="card-top-text">
+                            <div class="card-top-text-icon">
+                                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <circle cx="16.1477" cy="16.2939" r="15.5" fill="#C0C0C0" />
+                                    <path d="M18.1213 18.5185L16.6123 22.6683C16.5753 22.7679 16.5087 22.8538 16.4215 22.9145C16.3343 22.9751 16.2306 23.0076 16.1244 23.0076C16.0182 23.0076 15.9145 22.9751 15.8273 22.9145C15.7401 22.8538 15.6735 22.7679 15.6366 22.6683L14.1275 18.5185C14.1012 18.4469 14.0596 18.382 14.0057 18.3281C13.9518 18.2742 13.8869 18.2326 13.8153 18.2063L9.66545 16.6972C9.56587 16.6603 9.47998 16.5937 9.41933 16.5065C9.35868 16.4193 9.32617 16.3156 9.32617 16.2094C9.32617 16.1032 9.35868 15.9995 9.41933 15.9123C9.47998 15.8251 9.56587 15.7585 9.66545 15.7215L13.8153 14.2125C13.8869 14.1861 13.9518 14.1446 14.0057 14.0907C14.0596 14.0368 14.1012 13.9718 14.1275 13.9003L15.6366 9.75041C15.6735 9.65083 15.7401 9.56494 15.8273 9.50429C15.9145 9.44364 16.0182 9.41113 16.1244 9.41113C16.2306 9.41113 16.3343 9.44364 16.4215 9.50429C16.5087 9.56494 16.5753 9.65083 16.6123 9.75041L18.1213 13.9003C18.1477 13.9718 18.1892 14.0368 18.2431 14.0907C18.297 14.1446 18.362 14.1861 18.4335 14.2125L22.5834 15.7215C22.683 15.7585 22.7689 15.8251 22.8295 15.9123C22.8902 15.9995 22.9227 16.1032 22.9227 16.2094C22.9227 16.3156 22.8902 16.4193 22.8295 16.5065C22.7689 16.5937 22.683 16.6603 22.5834 16.6972L18.4335 18.2063C18.362 18.2326 18.297 18.2742 18.2431 18.3281C18.1892 18.382 18.1477 18.4469 18.1213 18.5185Z" fill="#C0C0C0" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
+                                <p class="title">BRONZE</p>
+                            </div>
+
+                            <p class="caption">Lorem Ipsum</p>
+                        </div>
+
+                        <div class="card-price-text">
+                            <p class="peso">₱</p>
+                            <p class="price">199</p>
+                            <p class="month">/month</p>
+                        </div>
+
+                        <button class="btn btn-primary btn-fill btn-contact">Subscribe
+                        </button>
+
+                        <hr>
+
+                        <div class="card-details-text-container">
+                            <div class="card-details-text">
+                                <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.2925 2.39282L5.11895 11.3265L2.47314 7.97636" stroke="#2FCF3F" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
+                                <p class="pre-title">Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+                            </div>
+                            <div class="card-details-text">
+                                <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.2925 2.39282L5.11895 11.3265L2.47314 7.97636" stroke="#2FCF3F" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
+                                <p class="pre-title">Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+                            </div>
+                            <div class="card-details-text">
+                                <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.2925 2.39282L5.11895 11.3265L2.47314 7.97636" stroke="#2FCF3F" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+
+                                <p class="pre-title">Lorem ipsum dolor sit amet consectetur adipiscing elit</p>
+                            </div>
+                        </div>
+                    </div>
+                </label>
+
+            </div>
+
+        </div>
+
 
     </div>
 </body>
